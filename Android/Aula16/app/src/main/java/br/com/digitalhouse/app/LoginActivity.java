fc8221a8b88e9ff548aad89c9d2b2a94bd4cb806
Roberto Stetter Netto <roberto@stetter.com.br>
2018-09-11 22:12:44 -0300

    revisao

--- Android/Aula16/app/src/main/java/br/com/digitalhouse/app/LoginActivity.java
+++ Android/Aula16/app/src/main/java/br/com/digitalhouse/app/LoginActivity.java
@@ -1,13 +1,27 @@
 package br.com.digitalhouse.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView textViewRegisterNow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textViewRegisterNow = (TextView) findViewById(R.id.textview_register);
+
+        textViewRegisterNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, CadastroActivity.class));
+            }
+        });
     }
 }
